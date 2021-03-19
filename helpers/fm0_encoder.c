2fc6113c868d982978eab715872d9d44352abe03
decoejz <decoejz@gmail.com>
2021-03-19 17:02:15 -0300

    change name

--- helpers/fm0_encoder.c
+++ helpers/fm0_encoder.c
@@ -22,21 +22,21 @@ static void fm0_encode1(int pin_tx, int tari){
 }
 
 /*!
- *  @brief Process a buffer to send through FM0 based encoder
+ *  @brief Process the package to send through FM0 based encoder
  *
- *  @param      buffer  Buffer pointer
- *  @param      size    Buffer size
+ *  @param      package  package pointer
+ *  @param      size    package size
  */
-void encode_FM0(char *buffer, uint32_t size, int pin_tx, int tari) {
+void encode_FM0(char *package, uint32_t size, int pin_tx, int tari) {
     // Value that helps to work bits
     char helper = 0x80;
     int value, i, info;
 
-    // Iterate through buffer
+    // Iterate through package
     for (info = 0; info < size; info++) {
         // Process byte info
         for (i=7; i>=0; i--) {
-            value = buffer[info] & helper;
+            value = package[info] & helper;
             value >>= i;
             if (value) {
                 fm0_encode1(pin_tx, tari);
