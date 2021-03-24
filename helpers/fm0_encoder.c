919742b5a451b3761218b06765bcfb4ec37a4668
decoejz <decoejz@gmail.com>
2021-03-24 19:10:00 -0300

    [REFAT] - FM0 encoder function

--- helpers/fm0_encoder.c
+++ helpers/fm0_encoder.c
@@ -27,25 +27,14 @@ static void fm0_encode1(int pin_tx, int tari){
  *  @param      package  package pointer
  *  @param      size    package size
  */
-void encode_FM0(char *package, uint32_t size, int pin_tx, int tari) {
-    // Value that helps to work bits
-    char helper = 0x80;
-    int value, i, info;
-
-    // Iterate through package
-    for (info = 0; info < size; info++) {
-        // Process byte info
-        for (i=7; i>=0; i--) {
-            value = package[info] & helper;
-            value >>= i;
-            if (value) {
-                fm0_encode1(pin_tx, tari);
-            } else {
-                fm0_encode0(pin_tx, tari);
-            }
-            helper>>=1;
+void encode_FM0(int package, int size, int pin_tx, int tari) {
+    int i;
+    for (i=(size-1); i>=0; i--) {
+        if ((package >> i) & 1) {
+        fm0_encode1(pin_tx, tari);
+		} else {
+        fm0_encode0(pin_tx, tari);
         }
-        helper = 0x80;
     }
     fm0_encode1(pin_tx, tari);
 }
