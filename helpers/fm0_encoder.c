8a778f7d42eb40399fe4879cf2c1b098b65524e7
decoejz <decoejz@gmail.com>
2021-03-24 19:35:34 -0300

    update function description

--- helpers/fm0_encoder.c
+++ helpers/fm0_encoder.c
@@ -5,6 +5,9 @@
 /************************************************************************/
 /*!
  *  @brief Send FM0 signal when bit value is low
+ * 
+ *  @param      pin_tx   pin board int to send the signal
+ *  @param      tari     tari value to properly send the signal
  */
 static void fm0_encode0(int pin_tx, int tari){
     GPIO_toggle(pin_tx);
@@ -15,6 +18,9 @@ static void fm0_encode0(int pin_tx, int tari){
 
 /*!
  *  @brief Send FM0 signal when bit value is high
+ * 
+ *  @param      pin_tx   pin board int to send the signal
+ *  @param      tari     tari value to properly send the signal
  */
 static void fm0_encode1(int pin_tx, int tari){
     GPIO_toggle(pin_tx);
@@ -25,7 +31,9 @@ static void fm0_encode1(int pin_tx, int tari){
  *  @brief Process the package to send through FM0 based encoder
  *
  *  @param      package  package pointer
- *  @param      size    package size
+ *  @param      size     package size
+ *  @param      pin_tx   pin board int to send the signal
+ *  @param      tari     tari value to properly send the signal
  */
 void encode_FM0(int package, int size, int pin_tx, int tari) {
     int i;
