b369ba23520e3b0ef7c33accce95c907e04b9b56
decoejz <decoejz@gmail.com>
2021-04-09 16:42:10 -0300

    fix fm0 decoder

--- helpers/fm0_decoder.c
+++ helpers/fm0_decoder.c
@@ -1,8 +1,5 @@
 #include "fm0_decoder.h"
 
-
-#include <ti/drivers/UART.h>
-
 /************************************************************************/
 /* functions                                                            */
 /************************************************************************/
@@ -13,35 +10,22 @@
  *  @param      tari    tari time
  *  @param      package package pointer
  */
-void fm0_decoder(int pin_rx , int tari ,int *package , UART_Handle uart) {
-    unsigned int package_pointer = 0;    // pointer of current data
+void fm0_decoder(int pin_rx , int tari ,int *package) {
     unsigned int actual = 0;        // current data signal
     unsigned int last = 0;          // last data signal
 
-
+    char condition_inter = 1;
     while(1){
         last = GPIO_read(pin_rx);
         usleep(tari/2); // microseconds
         actual = GPIO_read(pin_rx);
-        if(actual == last){
-            usleep(tari/2); // microseconds
-            
+        usleep(tari/2); // microseconds
+        *package<<1;
+        if(actual == last){            
             if(GPIO_read(pin_rx) == last){
-                break;
+            	break;
             }
-            UART_write(uart, "1", sizeof("1"));
-
-            package[package_pointer] = 1;  
+            *package |= 1;
         }
-        else{
-            usleep(tari/2); // microseconds
-            package[package_pointer] = 0;
-
-            UART_write(uart, "0", sizeof("0"));
-        }
-        package_pointer ++;
     }
-
-    UART_write(uart, "\r\n", sizeof("\r\n"));
-
 }
--- helpers/fm0_decoder.h
+++ helpers/fm0_decoder.h
@@ -4,10 +4,7 @@
 #ifndef FM0_DECODER_H
 #define FM0_DECODER_H
 
-
-#include <ti/drivers/UART.h>
-
-void fm0_decoder(int pin_rx, int tari,int *package , UART_Handle uart);
+void fm0_decoder(int pin_rx, int tari,int *package);
 
 #endif /* FM0_DECODER_H */
 
