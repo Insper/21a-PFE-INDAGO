c94cd8cd10a276b45dd38edca4432cd02c2bbf37
decoejz <decoejz@gmail.com>
2021-04-29 11:38:42 -0300

    update main.g

--- helpers/main.h
+++ helpers/main.h
@@ -1,6 +1,6 @@
 #include "config.h"
+#include "./drivers/drivers.h"
 #include "fm0_encoder.h"
 #include "fm0_decoder.h"
 #include "crc.h"
 #include "./commands/commands.h"
-#include "./drivers/drivers.h"
